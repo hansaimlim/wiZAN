b20c9bc2f1804d4163544b43adba84d19e90e416
Hansaim Lim <hansaimlim@gmail.com>
2015-05-22 14:13:02 -0400

    takes outfile as input argument

--- PRW_wiZAN/script/PRW_wiZAN_onetest.m
+++ PRW_wiZAN/script/PRW_wiZAN_onetest.m
@@ -1,19 +1,24 @@
-function [MAP, MPR, HLU, AUC] = PRW_wiZAN_dual(train, test, prw, user, item, para)
+function [MAP, MPR, HLU, AUC] = PRW_wiZAN_onetest(train_csv, test_csv, prw_csv, outfile, user, item, para)
 %modified version of wiZAN_dual for PRW_wiZAN process
 %takes 3rd input argument 'prw' which contains prw result for all unique chemicals against all proteins in training set
 %for single test pair (train, test, prw)
+%number of chemicals and proteins set for ZINC (12384, 3500)
 
-if nargin<6
+if nargin<7
     %default rank=300, p6=0.75, p7=0.1 modified on 5/19/2015
     para = [0.1, 300, 100, 0.75, 0.1]; % para: alpha, rank, maxIte, gamma, lambda
 end
 
 
+%convert csv to matrix
+train = sparse(train_csv(:,1), train_csv(:,2), 1, 12384, 3500);	%12384 chemicals and 3500 proteins in ZINC
+P=[prw_csv(:,1),prw_csv(:,2),prw_csv(:,3)];   %PRW results for test chemicals. Others are zeros
+test = csvread(test_csv);
+
 %item = ceil(item);
 user = user + user';
 %item = item + item';
 [m, n] = size(train);
-P=[prw(:,1),prw(:,2),prw(:,3)];   %PRW results for test chemicals. Others are zeros
 Pu=P.*(~train); %imPutation matrix. P(i, j) for test chemicals (only pairs NOT known). If a pair is known, value is 0. All others are zeros.
 W=train+Pu; %weight, 1 for train pairs
 
@@ -29,14 +34,18 @@ Lv = Dn - item;
 
 %get predicted scores and ranks based on updated U and V
 test_result = TPRbyRowRank(get_test_result(test, U, V), 200);	%max cutoff rank 200
-assignin('caller', 'test_results', test_result);
-%assignin('base', 'scores', scores);
 
 %[MPR_C, MPR_U, MPR_I] = get_diff_coldstart(train, test, U, V);
 [MAP, MPR, HLU, AUC] = get_diff(test, U, V, para);
 fprintf('MAP = %0.4f, MPR = %0.4f, HLU = %0.4f, AUC = %0.4f\n', MAP, MPR, HLU, AUC);
-%assignin('caller', 'AUC', AUC);
+fprintf('Result file saved: %s\n',outfile)
+save (outfile, 'test_result', '-ascii');
 
+clear train;
+clear test;
+clear test_result;
+clear P;
+clear para;
 end
 
 function test_result = get_test_result(test, U, V)
