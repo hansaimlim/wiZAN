9c74c77ef6d1a656bb0768123278dda5d4984317
Hansaim Lim <hansaimlim@gmail.com>
2015-08-27 19:15:12 -0400

    Number of rows corrected

--- wiZAN_dual/FindTrues.m
+++ wiZAN_dual/FindTrues.m
@@ -1,14 +1,11 @@
 function [row_col_rank_score] = FindTrues(A, TM) %A: predicted score matrix, TM: True Matrix (either true positive or true negative)
-si = (size(A));
-row_col_rank_score = zeros(sum(TM(:)),4);
-rcrs_count = 1;
 [i j k]=find(TM);
+row_col_rank_score = zeros(length(i),4);
 for n=1:length(i)
   row=i(n);
   col=j(n);
   predscore = A(row, col);
   drank = sum(A(row,:) >= A(row, col));
-  row_col_rank_score(rcrs_count,:) = [row, col, drank, predscore];
-  rcrs_count = rcrs_count + 1;
+  row_col_rank_score(n,:) = [row, col, drank, predscore];
 end 
 end
