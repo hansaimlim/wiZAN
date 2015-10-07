e107d59834bee438a475a609f85573ab952d73be
Hansaim Lim <hansaimlim@gmail.com>
2015-10-07 18:53:26 -0400

    printout normalized score pairs for all predictions

---
+++ ZINC_ChEMBL/results/normalize_score.m
@@ -0,0 +1,27 @@
+function normalize_score(normscore, P)
+%normscore: array of normalized scores ordered by bin
+%P: predicted raw score matrix
+filehandles=[];
+for i=1:size(normscore,1)
+ nscore=normscore(i);
+ filename=['./pairs_in_normScore_' num2str(nscore) '.txt'];
+ fh=fopen(filename, 'a');
+ filehandles=[filehandles, fh];
+end
+
+for i=1:size(P,1)
+ for j=1:size(P,2)
+  raw=20*P(i,j);
+  f=fix(raw);
+  if (raw-f)==0
+   nscore=normscore(f);
+   fh=filehandles(f);
+   fpritf(fh, '%d, %d\n', [i, j]);
+  else
+   nscore=normscore(f+1);
+   fh=filehandles(f+1);
+   fpritf(fh, '%d, %d\n', [i, j]);
+  end
+ end
+end
+end
