ab76ae51ec87db9bcc739fa979b8905e5b985f3c
Hansaim Lim <hansaimlim@gmail.com>
2015-10-07 23:48:50 -0400

    scores over 1.5 discarded

--- ZINC_ChEMBL/results/normalize_score.m
+++ ZINC_ChEMBL/results/normalize_score.m
@@ -4,14 +4,16 @@ function N=normalize_score(normscore, P)
 N=zeros(size(P,1),size(P,2));
 for i=1:size(P,1)
  for j=1:size(P,2)
-  raw=20*P(i,j);
-  f=fix(raw);
-  if (raw-f)==0
-   nscore=normscore(f);
-   N(i,j)=nscore;
-  else
-   nscore=normscore(f+1);
-   N(i,j)=nscore;
+  if P(i,j)<=1.5
+	  raw=20*P(i,j);
+	  f=fix(raw);
+	  if (raw-f)==0
+	   nscore=normscore(f);
+	   N(i,j)=nscore;
+	  else
+	   nscore=normscore(f+1);
+	   N(i,j)=nscore;
+	  end
   end
  end
 end
