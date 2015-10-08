499a225ce6fbd389fdaf529e0a1a188677f37c68
Hansaim Lim <hansaimlim@gmail.com>
2015-10-07 23:43:20 -0400

    returns matrix having normalized scores instead of raw scores

--- ZINC_ChEMBL/results/normalize_score.m
+++ ZINC_ChEMBL/results/normalize_score.m
@@ -1,26 +1,17 @@
-function normalize_score(normscore, P)
+function N=normalize_score(normscore, P)
 %normscore: array of normalized scores ordered by bin
 %P: predicted raw score matrix
-filehandles=[];
-for i=1:size(normscore,2)
- nscore=normscore(i);
- filename=['./pairs_in_normScore_' num2str(nscore) '.txt'];
- fh=fopen(filename, 'a');
- filehandles=[filehandles, fh];
-end
-
+N=zeros(size(P,1),size(P,2);
 for i=1:size(P,1)
  for j=1:size(P,2)
   raw=20*P(i,j);
   f=fix(raw);
   if (raw-f)==0
    nscore=normscore(f);
-   fh=filehandles(f);
-   fpritf(fh, '%d, %d\n', [i, j]);
+   N(i,j)=nscore;
   else
    nscore=normscore(f+1);
-   fh=filehandles(f+1);
-   fpritf(fh, '%d, %d\n', [i, j]);
+   N(i,j)=nscore;
   end
  end
 end
