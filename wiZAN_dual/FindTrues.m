5e2e2587034c1517c5f6b68ce4f3e5b6d960400b
Hansaim Lim <hansaimlim@gmail.com>
2016-02-15 01:18:39 -0500

    comment added

--- wiZAN_dual/FindTrues.m
+++ wiZAN_dual/FindTrues.m
@@ -1,11 +1,12 @@
-function [row_col_rank_score] = FindTrues(A, TM) %A: predicted score matrix, TM: True Matrix (either true positive or true negative)
+function [RCRS] = FindTrues(A, TM) %A: predicted score matrix, TM: Test Matrix (either true positive or true negative)
+%returns RCRS=[RowIndex, ColumnIndex, RowRank, PredScore]
 [i j k]=find(TM);
-row_col_rank_score = zeros(length(i),4);
+RCRS = zeros(length(i),4);
 for n=1:length(i)
   row=i(n);
   col=j(n);
   predscore = A(row, col);
-  drank = sum(A(row,:) >= A(row, col));
-  row_col_rank_score(n,:) = [row, col, drank, predscore];
+  drank = sum(A(row,:) >= A(row, col));	%dense rank on the row
+  RCRS(n,:) = [row, col, drank, predscore];
 end 
 end
