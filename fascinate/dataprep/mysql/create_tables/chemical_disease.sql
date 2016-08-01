ed43707277bebcbb81e0214e9c442b1297008565
Hansaim Lim <hansaimlim@gmail.com>
2016-08-01 18:12:53 -0400

    updated sql

--- fascinate/dataprep/mysql/create_tables/chemical_disease.sql
+++ fascinate/dataprep/mysql/create_tables/chemical_disease.sql
@@ -1,5 +1,6 @@
 CREATE TABLE chemical_disease(
+chemical_disease_index INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 chemical_index INT(10) UNSIGNED  NOT NULL,
 disease_index INT(10) UNSIGNED NOT NULL,
-PRIMARY KEY(chemical_index, disease_index),
+PRIMARY KEY(chemical_disease_index),
 UNIQUE KEY(chemical_index, disease_index));
